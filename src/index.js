7d2c6b7615f49d67eec7b25786d25a55bbe09660
Nick Nesterov <njnesterov@Nicks-MacBook-Pro.local>
2017-12-28 13:14:34 +0200

    added: scrolling of the tabs

--- src/index.js
+++ src/index.js
@@ -1,38 +1,63 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Animated, StyleSheet, Text, TouchableOpacity, View, ViewPropTypes } from "react-native";
+import { Animated, StyleSheet, Text, TouchableOpacity, View, ViewPropTypes, ScrollView } from "react-native";
 
 export default class NativeTabs extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      underlineWidthAm: new Animated.Value(0),
-      underlineOffsetAm: new Animated.ValueXY({x: 0, y: 0})
-    };
-    this.onLayout = this.onLayout.bind(this);
-    this.createActiveTabRef = this.createActiveTabRef.bind(this);
-
-    this.onTab = this.onTab.bind(this);
-    this.getUnderlineStyle = this.getUnderlineStyle.bind(this);
-    this.moveUnderline = this.moveUnderline.bind(this);
+  state = {
+    underlineWidthAm: new Animated.Value(0),
+    underlineOffsetAm: new Animated.ValueXY({x: 0, y: 0}),
+    tabViewWidth: null,
+    scrollViewWidth: null,
+    scrollStep: null
   }
-  onLayout(event) {
-    var { x, width } = event.nativeEvent.layout;
+  componentWillUpdate(nextProps, nextState) {
+    if (
+      nextProps.scrollEnabled &&
+      nextState.scrollStep === null &&
+      !!nextState.tabViewWidth &&
+      !!nextState.scrollViewWidth
+    ) this.saveStap(nextState.tabViewWidth, nextState.scrollViewWidth);
+  }
+  saveStap(tabViewWidth, scrollViewWidth) {
+    const scrollStep = (tabViewWidth - scrollViewWidth) / this.props.tabs.length;
+    this.setState({
+      scrollStep
+    });
+  }
+  onLayoutTabView = (event) => {
+    const { width } = event.nativeEvent.layout;
+    this.setState({
+      tabViewWidth: width
+    });
+  }
+  onLayoutScrollView = (event) => {
+    const { width } = event.nativeEvent.layout;
+    this.setState({
+      scrollViewWidth: width
+    });
+  }
+  onLayoutActiveTab = (event) => {
+    const { x, width } = event.nativeEvent.layout;
     this.setState({
       underlineWidthAm: new Animated.Value(width),
       underlineOffsetAm: new Animated.ValueXY({x: x, y: 0})
     });
   }
-  createActiveTabRef(c) {
+  createActiveTabRef = (c) => {
     if (c) c.measure((fx, _, width) => {
       this.moveUnderline({offset: fx, width: width});
     });
     this.activeTab = c;
   }
-  onTab(tab) {
+  onTab = (tab, i) => {
+    if (this.props.scrollEnabled) {
+      const move = this.state.scrollStep * i;
+      const x = this.props.tabs.length / 2 < i ? move + this.state.scrollStep : move;
+      this.scrollView.scrollTo({x: x, animated: true});
+    }
     if (!this.props.disabled) this.props.onTab(tab);
   }
-  getUnderlineStyle() {
+  getUnderlineStyle = () => {
     const t = this.state.underlineOffsetAm.getTranslateTransform();
     const propStyles = this.getStyles(this.props.styles)("underline");
     return [
@@ -43,7 +68,7 @@ export default class NativeTabs extends Component {
       }
     ]
   }
-  moveUnderline({offset, width}) {
+  moveUnderline = ({offset, width}) => {
     if (width) Animated.parallel([
       Animated.timing(this.state.underlineOffsetAm, {
         toValue: {x: offset, y: 0},
@@ -55,13 +80,13 @@ export default class NativeTabs extends Component {
       })
     ]).start();
   }
-  getStyles(styles) {
+  getStyles = (styles) => {
       return (c) => !!styles && !!styles[c] ? styles[c] : null
   }
   render() {
     const styleOf = this.getStyles(this.props.styles);
-    return (
-      <Animated.View style={[styles.tabs , styleOf("tabs")]}>
+    const Tabs = (
+      <Animated.View style={[styles.tabs , styleOf("tabs")]} onLayout={this.onLayoutTabView}>
         {
           this.props.tabs.map((tab, index) => {
 
@@ -73,7 +98,7 @@ export default class NativeTabs extends Component {
                 tab: styles.activeTab,
                 text: styles.activeTabText
               };
-              onLayout = this.onLayout;
+              onLayout = this.onLayoutActiveTab;
               refs = { ref: this.createActiveTabRef };
             } else {
               style = {
@@ -86,7 +111,7 @@ export default class NativeTabs extends Component {
             return (
               <TouchableOpacity key={index}
                 onLayout={onLayout}
-                onPress={this.onTab.bind(this, tab)} {...refs}
+                onPress={this.onTab.bind(this, tab, index)} {...refs}
                 style={[style.tab, styleOf("tab")]}>
                 <Text style={[style.text, styleOf("tabText")]}>{tab.name}</Text>
               </TouchableOpacity>
@@ -96,6 +121,28 @@ export default class NativeTabs extends Component {
         <Animated.View style={this.getUnderlineStyle()}/>
       </Animated.View>
     );
+
+    if (this.props.scrollEnabled) {
+      return (
+        <View style={[styles.wrap , styleOf("wrap")]}>
+          <ScrollView
+            contentContainerStyle={styles.scrollView}
+            directionalLockEnabled={true}
+            horizontal={this.props.scrollEnabled}
+            showsHorizontalScrollIndicator={false}
+            ref={ref => this.scrollView = ref}
+            onLayout={this.onLayoutScrollView}>
+            {Tabs}
+          </ScrollView>
+        </View>
+      );
+    } else {
+      return (
+        <View style={[styles.wrap , styleOf("wrap")]}>
+          {Tabs}
+        </View>
+      );
+    }
   }
 }
 
@@ -108,7 +155,9 @@ NativeTabs.propTypes = {
   }).isRequired,
   onTab: PropTypes.func,
   disabled: PropTypes.bool,
+  scrollEnabled: PropTypes.bool,
   styles: PropTypes.shape({
+    wrap: ViewPropTypes.style,
     tabs: ViewPropTypes.style,
     tab: TouchableOpacity.propTypes.style,
     tabText: Text.propTypes.style,
@@ -119,20 +168,25 @@ NativeTabs.propTypes = {
 }
 
 NativeTabs.defaultProps = {
-  disabled: false
+  disabled: false,
+  scrollEnabled: false
 }
 
 const styles = StyleSheet.create({
+  wrap: {
+    flexDirection: 'row',
+  },
+  scrollView: {
+    alignSelf: 'flex-start',
+  },
   tabs: {
     position: "relative",
     width: "100%",
     flexDirection: "row",
-    borderBottomColor: "#eaeaea",
-    borderBottomWidth: 1,
     justifyContent: "space-between"
   },
   tab: {
-    padding: 8
+    padding: 8,
   },
   tabText: {
     color: "#e1e1e1"
